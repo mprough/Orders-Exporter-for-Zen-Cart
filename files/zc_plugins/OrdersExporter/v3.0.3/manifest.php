36347020b5321e8102a77a2f2ba915abe3c791cf
Melanie Prough <mprough@pro-webs.net>
2026-09-02 08:12:24 -0400

    Fix admin menu bootstrap in v3.0.3

---
+++ files/zc_plugins/OrdersExporter/v3.0.3/manifest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+if (!defined('IS_ADMIN_FLAG')) {
+    die('Illegal Access');
+}
+
+return [
+    'pluginVersion' => 'v3.0.3',
+    'pluginName' => 'Orders Exporter',
+    'pluginDescription' => 'Stream order exports safely on stores of any size.',
+    'pluginAuthor' => 'PRO-Webs.net',
+    'pluginId' => 560,
+    'zcVersions' => ['v200', 'v210', 'v220'],
+    'changelog' => 'https://github.com/mprough/Orders-Exporter-for-Zen-Cart',
+    'github_repo' => 'https://github.com/mprough/Orders-Exporter-for-Zen-Cart',
+    'pluginGroups' => [],
+];
